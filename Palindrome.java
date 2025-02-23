a7f7b9033dc6ebf8fbd2b562a8f6c469f70e12a8
MADHANNVP <blackrose21082006@gmail.com>
2025-02-23 21:52:36 +0530

    add new file

---
+++ Palindrome.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Palindrome
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int x=s.nextInt();
+        int a;
+        // while(x>0)
+        // {
+            // }
+            x%=10;
+                a=x;
+        System.out.println(a);
+    }
+}
